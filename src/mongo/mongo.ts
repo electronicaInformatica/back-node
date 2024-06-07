851cc0e7901b0a80fe7439a4cc5419e64f492cf7
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2024-06-07 14:53:47 -0300

    fix mongo uri

--- src/mongo/mongo.ts
+++ src/mongo/mongo.ts
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 let DB_URL = process.env.DB_URL;
 let DB_PORT = process.env.DB_PORT;
-const mongoURI = 'mongodb://' + DB_URL + ':' + DB_PORT; // Change to your MongoDB URI
+let MONGO_DB = process.env.MONGO_DB
+const mongoURI = 'mongodb://' + DB_URL + ':' + DB_PORT + "/" + MONGO_DB; // Change to your MongoDB URI
 
 const mongoClient = new MongoClient(mongoURI);
 
